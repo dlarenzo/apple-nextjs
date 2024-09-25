0cd20f1faffd791180ec1b4af623d7e072b3d6ea
LaRenzo <dlarenzo@outlook.com>
2024-09-25 09:48:38 -0600

    comment

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,6 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      {/* test */}
       <body className={inter.className}>
         <Navbar />
         {children}
